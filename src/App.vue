261e41d47b0b51ac94151ad69e2d00a470869b7f
descholar-ceo <emmanuellamugi@gmail.com>
2021-06-27 23:58:16 +0200

    Passing props to components childrens

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-    <ul></ul>
+    <ul>
+        <learning-resources
+            v-for="res in storedResources" 
+            :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        ></learning-resources>
+    </ul>
 </template>
 
 <script>
+import LearningResources from './components/learning-resources/ResourceItem.vue';
 export default{
+    components: {
+        LearningResources,
+    },
     data() {
         return {
             storedResources: [
