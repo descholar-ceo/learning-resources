f1595bf3499a99ccbc2591544a8174676ce6a2b1
descholar-ceo <emmanuellamugi@gmail.com>
2021-06-27 23:19:27 +0200

    Add some learning resources dummy data

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul></ul>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official guide is documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'You need to learn to google ...',
+                    link: 'https://google.com'
+                },
+            ]
+        }
+    },
+}
+</script>
