a2f4057263bf823e4b897073213b37f78b028e0c
descholar-ceo <emmanuellamugi@gmail.com>
2021-06-28 00:08:31 +0200

    Add stylings

--- src/App.vue
+++ src/App.vue
@@ -36,3 +36,19 @@ export default{
     },
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
