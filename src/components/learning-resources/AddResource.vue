0eff8c01eb6394939576da075f92bb58567cc5ba
descholar-ceo <emmanuellamugi@gmail.com>
2021-06-28 01:40:54 +0200

    Add add-resource form

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,3 +1,49 @@
 <template>
-    <h2>Add resource</h2>
+    <base-card>
+        <form>
+            <div class="form-control">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title"/>
+            </div>
+            <div class="form-control">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" rows="3"></textarea>
+            </div>
+            <div class="form-control">
+                <label for="link">Link</label>
+                <input type="url" name="link" id="link"/>
+            </div>
+            <div class="form-control">
+                <base-button type="submit">Add resource</base-button>
+            </div>
+        </form>
+    </base-card>
 </template>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
